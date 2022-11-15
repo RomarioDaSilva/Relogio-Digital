04d36feae9808541301411a2caef7e4a902f8a4e
Romario <romario2656@gmail.com>
2022-11-15 18:25:28 -0300

    Novo SCRIPT e CSS

--- scripts.js
+++ scripts.js
@@ -1,50 +1,37 @@
-function relogio() {
-    const data = new Date()
+const horas = document.getElementById('horas')
+const minutos = document.getElementById('minutos')
+const segundos = document.getElementById('segundos')
 
-    let hora = data.getHours()
-    let minutos = data.getMinutes()
-    let segundos = data.getSeconds()
+const horaNewYork = document.getElementById('newYourk')
+const horadubai = document.getElementById('dubai')
+const horacancun = document.getElementById('cancun')
+const horalisboa = document.getElementById('lisboa')
+
+const relogio = setInterval(function time(){
+    let data = new Date();
+
+    let hr = data.getHours();
+    let min = data.getMinutes();
+    let seg = data.getSeconds();
+
+    if (hr < 10) hr = '0' + hr
+    if (min < 10) min = '0' + min
+    if (seg < 10) seg = '0' + seg
+
+    let newYork = (hr - 2) + ':' + min + ':' + seg;
+    let dubai = (hr + 7) + ':' + min + ':' + seg;
+    let cancun = (hr - 2) + ':' + min + ':' + seg;
+    let lisboa = (hr + 3) + ':' + min + ':' + seg;
 
 
-    if (hora < 10) {
-        if (minutos < 10) {
-            if (segundos < 10) {
-                document.getElementById('relogioDigital').innerHTML = '0' + hora + ':0' + minutos + ':0' + segundos
-            } else {
-                document.getElementById('relogioDigital').innerHTML = '0' + hora + ':0' + minutos + ':' + segundos
-            }
-        } else {
-            if (segundos < 10) {
-                document.getElementById('relogioDigital').innerHTML = '0' + hora + ':' + minutos + ':0' + segundos
-            } else {
-                document.getElementById('relogioDigital').innerHTML = '0' + hora + ':' + minutos + ':' + segundos
-            }
-        }
-    } else {
-        if (minutos < 10) {
-            if (segundos < 10) {
-                document.getElementById('relogioDigital').innerHTML = hora + ':0' + minutos + ':0' + segundos
-            } else {
-                document.getElementById('relogioDigital').innerHTML = hora + ':0' + minutos + ':' + segundos
-            }
-        } else {
-            if (segundos < 10) {
-                document.getElementById('relogioDigital').innerHTML = hora + ':' + minutos + ':0' + segundos
-            } else {
-                document.getElementById('relogioDigital').innerHTML = hora + ':' + minutos + ':' + segundos
-            }
-        }
-    }
+    horas.textContent = hr;
+    minutos.textContent = min;
+    segundos.textContent = seg;
 
-    if (hora >= 0 && hora < 6) {
-        document.getElementById('mensagem').innerHTML= "Boa madrugada!"
-    } else if (hora > 6 && hora < 12) {
-        document.getElementById('mensagem').innerHTML= "Bom Dia!"
-    }else if(hora > 12 && hora < 18) {
-        document.getElementById('mensagem').innerHTML= "Boa Tarde!"
-    }else {
-        document.getElementById('mensagem').innerHTML= "Boa Noite!"
-    }
-}
-
-setInterval(relogio, 1000)
+    horaNewYork.textContent = newYork;
+    horadubai.textContent = dubai;
+    horacancun.textContent = cancun;
+    horalisboa.textContent = lisboa;
+    
+    
+})
